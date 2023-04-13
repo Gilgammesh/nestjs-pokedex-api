ea098707c65237f64c64ada28accf22b1889f308
Carlos Santander <carlos.santander@gmail.com>
2023-04-12 19:38:35 -0500

    Corregir Variables de Entorno

--- src/config/app.validation.ts
+++ src/config/app.validation.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 
 export const AppValidationSchema = Joi.object({
-  APP_PORT: Joi.number().default(3000).required(),
+  PORT: Joi.number().default(3000).required(),
   MONGO_DB_CONNECTION_STRING: Joi.string().required()
 });
