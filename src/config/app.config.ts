018bd8933be0da4900fa7fc06c7647da309d41c4
Carlos Santander <carlos.santander@gmail.com>
2023-04-12 19:50:19 -0500

    Corregir Variables de Entorno

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,7 @@
 export const AppConfig = () => ({
-  environment: process.env['NODE_ENV'],
-  appPort: +process.env['PORT'],
-  mongoDbConnectionString: process.env['MONGO_DB_CONNECTION_STRING'],
+  environment: process.env.NODE_ENV,
+  appPort: +process.env.PORT,
+  mongoDbConnectionString: process.env.MONGO_DB_CONNECTION_STRING,
   appGlobalPrefix: 'api',
   defaultLimit: 1,
   defaultOffset: 10
